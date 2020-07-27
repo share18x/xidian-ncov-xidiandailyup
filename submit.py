386d00b40e83967ea812f07065ec1fb1e9005f0f
园田优 <share18x@gmail.com>
2020-07-27 12:47:30 +0800

    更新路径获取函数，更新日志函数

--- submit.py
+++ submit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
+
 import json
 import requests
 import re
@@ -7,14 +8,23 @@ import sys
 import os
 import time
 
+# output log
+def print_output_log():
+    print("正在记录日志中......\n")
+    output_str = "最终输出：\n"
+    output_str += str(predef) + "\n"
+    output_str += result.text + "\n"
+    output_str += time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime())
+    with open(currentdir + "/output.log", "a") as fd:
+        fd.write(output_str)
+
 if os.path.exists("NOSUBMIT"):
     exit()
 
 data = {}
 
-currentdir = os.getcwd()
-#print(currentdir + "\\data.json")
-with open(currentdir + "\\data.json","r") as fd:
+currentdir = os.path.dirname(os.path.abspath(__file__))
+with open(currentdir + "\\data.json") as fd:
     data=json.load(fd)
     
 conn = requests.Session()
@@ -31,15 +41,18 @@ if result.status_code != 200:
     print('获取页面大失败')
     exit()
 
-with open("last_get.html","w") as fd:
-    fd.write(result.text)
+# print("打印正则调试：\n%s" % re.search('"info":({.*}),"ontime"',result.text).group(1))
+# print("打印输入：\n" + str(data))
+
 
 #读取网页记录
 predef = json.loads(re.search('"info":({.*}),"ontime"',result.text).group(1))
+# print("打印上次提交记录：\n"+ str(predef))
 
-# if "dump_geo" in sys.argv:
-#     print(predef['geo_api_info'])
-#     exit()
+
+if "dump_geo" in sys.argv:
+    print(predef['geo_api_info'])
+    exit()
 
 try:
     del predef['jrdqtlqk']
@@ -56,8 +69,15 @@ del predef['created']
 del predef['id']
 predef.update(data)
 
-print("最终输出：\n" + str(predef))
+#最终测试
+# while True:
+#     select = input("真的要提交吗？\n请输入YES\\NO\n")
+#     if select == "YES":
+#         break
+#     elif select == "NO":
+#         exit()
 
 result = conn.post('https://xxcapp.xidian.edu.cn/xisuncov/wap/open-report/save',data=predef)
-print(result.text)
+
+print_output_log()
 time.sleep(10)
